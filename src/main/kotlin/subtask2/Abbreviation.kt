a972b2ea62da15331205763ff12ddd013965f608
Pavel Sidorovich <sidorovichpavelalex@gmail.com>
2021-06-10 20:53:33 +0300

    feat: implements tasks

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -2,8 +2,26 @@ package subtask2
 
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val from = a.toCharArray().toMutableList()
+        val to = b.toCharArray().toMutableList()
+
+        var count = 0
+
+        for (cTo in to) {
+            while (from.size != 0) {
+                if (from[0] == cTo || from[0].toUpperCase() == cTo) {
+                    count ++
+                    break
+                }
+                from.removeAt(0)
+            }
+        }
+        return if (count == to.size) {
+            "YES"
+        } else {
+            "NO"
+        }
+
     }
 }
